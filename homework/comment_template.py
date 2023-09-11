971364e9108f50d64afe7bb7acc191b5ad1a4412
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-09-11 12:13:02 -0400

    day 2 notes

--- homework/comment_template.py
+++ homework/comment_template.py
@@ -9,5 +9,7 @@ On my honor, I have neither given nor received unauthorized aid on this assignme
 """
 Notes on Sources
 If no source, still say so!
+If you used a person, say so!
+If you used a website, say so! Except for Python.org
 """
 
