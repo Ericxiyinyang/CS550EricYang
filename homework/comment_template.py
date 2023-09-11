1f398583ebf3c24a09eca4ef410dbb72ad805368
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-09-11 12:09:36 -0400

    day 2 notes

--- homework/comment_template.py
+++ homework/comment_template.py
@@ -1,1 +1,13 @@
-# On my honor, I have neither given nor received unauthorized aid on this assignment.
+"""
+Eric
+Date
+Sources * see notes
+Reflection
+On my honor, I have neither given nor received unauthorized aid on this assignment.
+"""
+
+"""
+Notes on Sources
+If no source, still say so!
+"""
+
