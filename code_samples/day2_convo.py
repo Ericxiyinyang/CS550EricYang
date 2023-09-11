cfc8a4b75a81a00073def5dba63300cd62a1a113
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-09-11 12:34:16 -0400

    day 2 notes

---
+++ code_samples/day2_convo.py
@@ -0,0 +1,7 @@
+from art import tprint
+
+print("Hello world!")
+first_name = input("What is your name? \n>>>")
+print(f"Hello! {first_name}. Nice to meet you!")
+# print("Hello!")
+# tprint(first_name)
