38d247b45873208e35a7d76312227ffddb9eb339
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-09-11 12:50:33 -0400

    day 2 notes

--- code_samples/day2_convo.py
+++ code_samples/day2_convo.py
@@ -5,3 +5,10 @@ first_name = input("What is your name? \n>>>")
 print(f"Hello! {first_name}. Nice to meet you!")
 # print("Hello!")
 # tprint(first_name)
+location = input("Where are you from? (town) \n>>>")
+location.lower().strip()
+
+if location == "vancouver" or "van":
+    print("No way! I'm from Vancouver too!")
+else:
+    print("Nice to know!")
