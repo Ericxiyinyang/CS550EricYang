39801606ca91bdda05ce4d654e5850a27b2f01e4
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-09-11 12:16:48 -0400

    day 2 notes

--- homework/comment_template.py
+++ homework/comment_template.py
@@ -9,8 +9,15 @@ On my honor, I have neither given nor received unauthorized aid on this assignme
 """
 Notes on Sources
 If no source, still say so!
+Sources: None
+
 If you used a person, say so!
+My brother helped me with explaining how Depth-First Search works
+
 If you used a website, say so! Except for Python.org
+I looked up how to use the random module on: w3schools.com/python/module_random.asp
+
 GPT should be cited as a source
+Description like above but reference the AI tool
 """
 
