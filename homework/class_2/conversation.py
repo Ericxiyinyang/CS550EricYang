be157bfde6db4652d20cb650d9d39975a131f3ed
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-09-12 18:20:29 -0400

    day 2 notes

--- homework/class_2/conversation.py
+++ homework/class_2/conversation.py
@@ -8,6 +8,7 @@ I used Textblob for basic sentimental analysis to aid in the creation of the inf
 https://textblob.readthedocs.io/en/dev/quickstart.html#sentiment-analysis
 Polarity score reading I did to learn more about how to set my polarity thresholds for positive and negative
 emotions: https://www.red-gate.com/simple-talk/development/data-science-development/sentiment-analysis-python/
+I used ChatGPT to generate the different story cutout templates with empty names and adjectives
 
 
 I think this is a great introductory assignment, I did something similar to this during spring of last year
@@ -33,10 +34,26 @@ class Greeter:
                                  "What are your feelings about school?", "Describe how you are feeling right now.",
                                  "Describe how tired you have been in the last week?", "How was your weekend?",
                                  "How are your friends?"]
+        self.random_jokes = ["What do you call a fake noodle?",
+                             "What do you call an alligator in a vest?",
+                             "What do you call a pile of cats?",
+                             "What do you call a pig that does karate?",
+                             "What do you call a cow with no legs?",
+                             "What do you call a cow with two legs?",
+                             "What do you call a cow with all legs?",
+                             "What do you call a cow that just gave birth?"]
+        self.random_jokes_ans = ["Impasta!", "An Investigator!", "A Meowtain!", "A Pork Chop!", "Ground Beef!",
+                                 "Lean Beef!", "High Steaks!", "De-calf-inated!"]
 
     def interaction(self):
-        print(random.choice(self.random_questions))
-        print(self.analyze_emotion(input(">>>")))
+        interaction_choice = input("1. Get a joke, 2. Get a random question, 3. Get a really, really horrible story XD")
+        if interaction_choice == 1:
+            self.give_joke()
+        elif interaction_choice == 2:
+            self.give_rand_question()
+        elif interaction_choice == 3:
+            pass
+
 
     def analyze_emotion(self, response):
         blob_analyzer = TextBlob(response)
@@ -64,6 +81,26 @@ class Greeter:
             nickname = f"{front_two_head}{rand_suffix}"
             return nickname.capitalize()
 
+    def give_joke(self):
+        # we need an index number to get the joke ans so we cant just use random.choice()
+        random_joke_index = random.randint(0, len(self.random_jokes))
+        print(self.random_jokes[random_joke_index])
+        time.sleep(0.8)
+        print("Think you got the answer? Press enter to see!")
+        input()
+        print(self.random_jokes_ans[random_joke_index])
+
+    def give_rand_question(self):
+        print(random.choice(self.random_questions))
+        print(self.analyze_emotion(input(">>>")))
+
+    def y_n_handler(self):
+        choice = input("(y/n)\n").lower
+        if choice == "y":
+            return True
+        else:
+            return False
+
 
 if __name__ == "__main__":
     first_name = input("Hello! What is your first name?\n>>>")
@@ -83,6 +120,9 @@ if __name__ == "__main__":
     else:
         print("Nice to know!")
     time.sleep(2)
+    want_joke = input("Do you want to hear a joke? (y/n)\n>>>").lower()
+    if want_joke == "y":
+
     print("I'm going to ask you some random questions now!")
     while True:
         main_greeter.interaction()
